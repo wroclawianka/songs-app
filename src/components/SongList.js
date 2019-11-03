2e8c6898b5c04e8da65a26e1815ce593250a78aa
dorota <dorota.zelga@gmail.com>
2019-11-03 16:20:14 +0100

    SongList > connect used

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 class SongList extends React.Component {
     render() {
@@ -8,4 +9,11 @@ class SongList extends React.Component {
     }
 }
 
-export default SongList;
+const mapStateToProps = (state) => { // function mapStateToProps named according to convention
+// we will take our state (all data we keep in redux store) and make some calculation on it
+    return state;
+};
+
+export default connect(mapStateToProps)(SongList);
+/*connect is function which returns a function,
+so in second brackets you see arguments of returned function*/
