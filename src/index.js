97580f2f780eee3b0db9ec59796903ef6d3a39e7
dorota <dorota.zelga@gmail.com>
2019-11-03 16:06:27 +0100

    provider added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from "redux"; // named export example
 import App from './components/App';
+import reducers from './reducers'; // default export example
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App/>
+    </Provider>,
+    document.querySelector('#root')
+);
