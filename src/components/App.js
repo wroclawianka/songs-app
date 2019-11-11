62e9f02bb44164ec01799b35150089de66dc96ea
dorota <dorota.zelga@gmail.com>
2019-11-11 16:31:16 +0100

    bit of styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import '../index.css';
 import SongList from "./SongList";
 import SongDetail from "./SongDetail";
 
 const App = () => {
     return (
-        <div className="ui container grid">
-            <div className="ui row">
-                <div className="column eight wide">
-                    <SongList/>
+        <div className="main-container">
+            <div className="ui container grid">
+                <h1 className="ui row">Your song picker</h1>
+                <div className="ui row">
+                    <div className="column six wide">
+                        <SongList/>
+                    </div>
+                    <div className="column six wide">
+                        <SongDetail/>
+                    </div>
                 </div>
             </div>
-            <div className="column eight wide">
-                <SongDetail/>
-            </div>
-      </div>
+        </div>
     );
 };
 
