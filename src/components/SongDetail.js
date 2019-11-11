5bd7cbadfe65a0c88f268fc97b0f527bc75bfe69
dorota <dorota.zelga@gmail.com>
2019-11-11 16:20:47 +0100

    Song Detail displayed

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail = () => {
-    return <div>Song Detail</div>
+const SongDetail = ({song}) => {
+    if (song) {
+        return <div>
+            <p>Title: {song.title}</p>
+            <p>Duration: {song.duration}</p>
+        </div>
+    } else {
+        return <div>Please, select song</div>
+    }
 };
 
 const mapStateToProps = (state) => {
     return {song: state.selectedSong}
 };
 
-export default connect()(SongDetail);
+export default connect(mapStateToProps)(SongDetail);
